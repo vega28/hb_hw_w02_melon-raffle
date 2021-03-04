29598864b7205d01268327c6b3afc67c9a8c886d
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-03-04 04:51:58 +0000

    Completed new version of raffle.py. Now more modular and does not require interactive mode to run.

--- raffle.py
+++ raffle.py
@@ -1,5 +1,11 @@
 """Read customer data from file and run a raffle."""
 
+# Noted issues:
+#   customer processing in same file as raffle itself
+#       break these up into separate modules
+#   must be run in interactive mode
+#       restructure so it can be run from the terminal
+
 from random import choice
 
 
@@ -42,7 +48,7 @@ def get_customers_from_file(customer_file_path):
 def pick_winner(customers):
     """Choose a random winner from list of customers."""
 
-    chosen_customer = random.choice(customers)
+    chosen_customer = choice(customers)
     
     name = chosen_customer.name
     email = chosen_customer.email
--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,25 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+# Import modules:
+from random import choice
+from customers import *
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+# raffle functions:
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customers)
+    
+    name = chosen_customer.name
+    email = chosen_customer.email
+
+    print(f"Tell {name} at {email} that they've won")
+
+
+def run_raffle():
+    """Run the weekly raffle from the terminal."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
+
+run_raffle()
